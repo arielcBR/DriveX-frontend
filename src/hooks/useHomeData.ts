eff5fdb746efa06ef70588ec7abe5df857ddd979
jacksonnnervis <jacksonnervis@gmail.com>
2026-06-19 20:42:04 -0300

    finaliza integração e correções na tela

--- src/hooks/useHomeData.ts
+++ src/hooks/useHomeData.ts
@@ -1,20 +1,23 @@
-import { useEffect, useState } from "react";
 import { fetchHomeData } from "@/services/homeServices";
 import { HomeData } from "@/types/home";
+import { useEffect, useState } from "react";
 
-export function useHomeData(idUsuario: number) {
+export function useHomeData() {
   const [data, setData] = useState<HomeData | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
-  useEffect(() => {
+  const fetchData = () => {
     setLoading(true);
-
-    fetchHomeData(idUsuario)
+    fetchHomeData()
       .then((resultado) => setData(resultado))
       .catch((erro) => setError(erro))
       .finally(() => setLoading(false));
-  }, [idUsuario]); 
+  };
 
-  return { data, loading, error };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { data, loading, error, refetch: fetchData };
 }
